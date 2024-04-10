6c6f27f81ce476b9331d400067bd583ed4173c2c
josores2 <josesorliquart@gmail.com>
2024-04-10 20:01:38 +0200

    Initial Commit

---
+++ SwiftUIMasterCounter/SwiftUIMasterCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMasterCounterApp.swift
+//  SwiftUIMasterCounter
+//
+//  Created by Simon Ng on 17/8/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMasterCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
